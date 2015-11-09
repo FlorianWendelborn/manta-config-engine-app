f36458401e701f4f03c38c2f4d1d32e7fd6e93e3
Florian Wendelborn <florianwendgithub@gmail.com>
2015-11-09 23:44:26 +0100

    fixed typo

--- web/source/components/bind-changer.jsx
+++ web/source/components/bind-changer.jsx
@@ -79,7 +79,7 @@ var Component = React.createClass({
 										<option value="2">3</option>
 										<option value="3">4</option>
 										<option value="4">5</option>
-										<option value="5">Ultimate</option>
+										<option value="5">6</option>
 									</select>
 									<br/>
 									Cast mode:
