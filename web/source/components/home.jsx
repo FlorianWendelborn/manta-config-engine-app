521035607bbca61911935c709e863aeab7f3e199
Florian Wendelborn <florianwendgithub@gmail.com>
2015-11-10 17:25:44 +0100

    changed link

--- web/source/components/home.jsx
+++ web/source/components/home.jsx
@@ -5,7 +5,9 @@ var Component = React.createClass({
 				<div className="jumbotron">
 					<h1>Manta Config Engine App</h1>
 					<p>Graphical Userinterface to generate autoexec files for Dota 2.</p>
-					<p><a className="btn btn-primary btn-lg" href="https://github.com/dodekeract/manta-config-engine-app" role="button">View GitHub</a></p>
+					<p>
+						<a className="btn btn-success btn-lg" href="#/editor">Start Editing</a>
+					</p>
 				</div>
 				<h3>Usage:</h3>
 				<ul>
@@ -16,8 +18,9 @@ var Component = React.createClass({
 					<li>Restart Dota 2 or enter "exec autoexec.cfg" into your console.</li>
 					<li>You may need to remove all keybindings for this to work.</li>
 				</ul>
-				<h3>Library:</h3>
+				<h3>GitHub Repositories:</h3>
 				<ul>
+					<li><a href="https://github.com/dodekeract/manta-config-engine-app">view This App on GitHub</a></li>
 					<li><a href="https://github.com/dodekeract/manta-config-engine">view JavaScript library on GitHub</a></li>
 				</ul>
 			</div>
